3845a6479535a5b6124b46ff3bebaca091276af8
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-31 17:56:45 -0300

    Created EventPositionInLocalCoordinates

---
+++ Assets/CharacterController/Scripts/EventPositionInLocalCoordinates.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/***
+ * All EventSystem events positions are in Screen Coordinates. This class calculates
+ * the event position in a local system whose origin is rectTransformPositionInScreenCoordinates.
+ * From it's name can be deduced that the main use will be to calculate the event position relative
+ * to a RectTransform.
+*/
+public class EventPositionInLocalCoordinates 
+{
+    public Vector2 Calculate(Vector2 rectTransformPositionInScreenCoordinates, Vector2 eventInScreenCoordinates)
+    {
+        Vector2 eventLocalCoordinates = eventInScreenCoordinates - rectTransformPositionInScreenCoordinates;
+        return eventLocalCoordinates;
+    }
+}
---
+++ Assets/Tests/CharacterController/EventPositionInLocalCoordinatesTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EventPositionInLocalCoordinatesTest
+    {
+
+        [Test]
+        public void CanConvertScreenCoordinatesToLocalCoordinatesInsideARect()
+        {
+            EventPositionInLocalCoordinates calculator = new EventPositionInLocalCoordinates();
+            Vector2 rectTransformPositionInScreenCoordinates = new Vector2(10, 10);
+            Vector2 eventInScreenCoordinates = new Vector2(15, 15);
+            Vector2 result = calculator.Calculate(rectTransformPositionInScreenCoordinates, eventInScreenCoordinates);
+            Assert.AreEqual(new Vector2(5, 5), result);
+        }
+
+    }
+}
