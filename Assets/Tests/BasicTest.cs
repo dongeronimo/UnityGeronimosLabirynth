5fe4305de321b68d9d9a3e23f5d1784e02c697b7
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-31 12:51:22 -0300

    Wrote my first test

---
+++ Assets/Tests/BasicTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BasicTest
+    {
+        [Test]
+        public void CanAssertSomething()
+        {
+            bool foo = false;
+            Assert.AreEqual(false, foo);
+        }
+    }
+}
