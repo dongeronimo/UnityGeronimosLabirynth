601b04185f582f573d1531fe8fa0665b3e0e30f2
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-31 14:12:51 -0300

    Removed the setUp and tearDown from BasicTest, they were breaking the test in when running in play mode. Added the MoveCubeTest and this test isn't passing because it is not implemented

--- Assets/Tests/BasicTest.cs
+++ Assets/Tests/BasicTest.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace Tests
@@ -13,6 +15,13 @@ namespace Tests
         {
             bool foo = false;
             Assert.AreEqual(false, foo);
+        }
+        [Test]
+        public void CanCatchErrors()
+        {
+            GameObject foo = new GameObject("test");
+            //Foo has no rigidbody and here i assert that the exception that I expect is being thrown where I expect.
+            Assert.Throws<MissingComponentException>(() => foo.GetComponent<Rigidbody>().velocity = Vector3.one);
         }
     }
 }
