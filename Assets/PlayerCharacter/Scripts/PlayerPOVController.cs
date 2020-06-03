2e91b869f1b2bc46dc5a725ce6f1f502622f7bad
luciano <luciano.geronimo.fnord@gmail.com>
2020-06-03 14:16:15 -0300

    The first part of player POV.

---
+++ Assets/PlayerCharacter/Scripts/PlayerPOVController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPOVController : MonoBehaviour
+{
+    public Transform SceneCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneCamera.position = this.transform.position;
+        SceneCamera.LookAt(transform.parent.position);
+        Debug.Log(SceneCamera.forward);
+    }
+}
---
+++ Assets/Tests/PlayerCharacter/PlayerPOVControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerPOVControllerTests
+    {
+
+        [UnityTest]
+        public IEnumerator CameraPositionChangesIfParentChanges()
+        {
+            throw new System.Exception("Not implemented");
+        }
+        [UnityTest]
+        public IEnumerator CameraPositionAndOrientationIsCorrectlySetInUpdate()
+        {
+            //Creates the camera
+            GameObject camera = new GameObject();
+            //Creates the parent
+            GameObject parent = new GameObject();
+            parent.transform.position = new Vector3(0, 0, 0);
+            //Creates the POV 
+            GameObject playerPOV = new GameObject();
+            playerPOV.transform.position = new Vector3(0f, 1.59f, -3.82f);
+            playerPOV.transform.parent = parent.transform;
+            playerPOV.AddComponent<PlayerPOVController>();
+            playerPOV.GetComponent<PlayerPOVController>().SceneCamera = camera.transform;
+            //skip a frame to process, since the change happens inside Update method.
+            yield return null;
+            //do the tests
+            var cameraPosition = camera.transform.position;
+            var orientation = camera.transform.forward;
+            Assert.AreEqual(cameraPosition, playerPOV.transform.position);
+            var orientationXEpsilon = Mathf.Abs(Mathf.Abs(orientation.x) - Mathf.Abs(0));
+            var orientationYEpsilon = Mathf.Abs(Mathf.Abs(orientation.y) - Mathf.Abs(-0.384f));
+            var orientationZEpsilon = Mathf.Abs(Mathf.Abs(orientation.z) - Mathf.Abs(-0.923f));
+            Assert.Less(orientationXEpsilon, 0.001f);
+            Assert.Less(orientationYEpsilon, 0.001f);
+            Assert.Less(orientationZEpsilon, 0.001f);
+            yield return null;
+        }
+    }
+}
