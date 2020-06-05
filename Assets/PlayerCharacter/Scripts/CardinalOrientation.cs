f64dce5231e22fc9aabe464f181cd4520e0e8fe2
luciano <luciano.geronimo.fnord@gmail.com>
2020-06-04 21:18:51 -0300

    Solved the issue: By using spherical interpolation between the current orientation and the orientation that came from the joystick to interpolate by a certain amount (began with 0.1, that means 10%) of the distance each update cycle and taking this interpolated value and applying to the character rotation I solved the issue and implemented freefire-like rotation

--- Assets/PlayerCharacter/Scripts/CardinalOrientation.cs
+++ Assets/PlayerCharacter/Scripts/CardinalOrientation.cs
@@ -6,26 +6,22 @@ public class CardinalOrientation : MonoBehaviour
 {
     public Vector3 currentOrientation = new Vector3(0,0,1);
     public Vector3 finalOrientation;
-   // public float speed = 1;
-   // public float startTime;
+
     public JoystickServices joystickServices;
-    // Start is called before the first frame update
+
     void Start()
     {
-    //    startTime = Time.time;
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         SetXAndYUsingJoystickServices();
-        float angleBetweenCurrentAndFinalOrientations = Vector3.Angle(currentOrientation, finalOrientation);
-        Debug.Log("angle = " + angleBetweenCurrentAndFinalOrientations);
-        //Debug.Log("Angle between vectors = " + angleBetweenCurrentAndFinalOrientations);
-        //Vector3.Lerp()
-    //    ChangeOrientationBasedOnXAndY();
-        //Debug.Log("CurrentOrientation = " + currentOrientation);
-        //Debug.Log("FinalOrientation = " + finalOrientation);
+        Vector3 interpolatedPosition = Vector3.Slerp(currentOrientation, finalOrientation, 0.1f);
+        //Debug.Log("current = " + currentOrientation + ", intermediate = " + interpolatedPosition + ", final = " + finalOrientation);
+        currentOrientation = interpolatedPosition;
+        ChangeOrientationBasedOnXAndY();
     }
     private void SetXAndYUsingJoystickServices()
     {
@@ -33,15 +29,13 @@ public class CardinalOrientation : MonoBehaviour
         {
             var x = joystickServices.CurrentMovementAxes.x;
             var z = joystickServices.CurrentMovementAxes.y;
-            //Debug.Log("x,y = " + x+","+z);
             finalOrientation = new Vector3(x, 0, z);
         } 
     }
     private void ChangeOrientationBasedOnXAndY()
     {
-        //var currentPosition = transform.position;
-        //var positionToLookAt = currentPosition + currentOrientation;
-        //transform.LookAt(positionToLookAt);
-               
+        var currentPosition = transform.position;
+        var positionToLookAt = currentPosition + currentOrientation;
+        transform.LookAt(positionToLookAt);
     }
 }
