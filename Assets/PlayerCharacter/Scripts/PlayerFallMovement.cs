cb279ed5c1c80031e131a33916e0680b380f956f
dongeronimo <luciano.geronimo.fnord@gmail.com>
2020-06-07 13:30:57 -0300

    Added a fall animation. Will have to rethink the animation state machine because it isn't working as intended

--- Assets/PlayerCharacter/Scripts/PlayerFallMovement.cs
+++ Assets/PlayerCharacter/Scripts/PlayerFallMovement.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerFallMovement : MonoBehaviour
 {
     private CharacterController characterController;
+    private Animator animator;
     private Vector3 lastFallVector;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        animator.SetFloat("FallSpeed", lastFallVector.y);
         if (characterController.isGrounded == false)
         {
             lastFallVector = Vector3.Slerp(lastFallVector, Vector3.down, 0.1f);
