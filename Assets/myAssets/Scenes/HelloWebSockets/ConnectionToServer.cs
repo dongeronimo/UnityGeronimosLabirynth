0cab197a8ea544b20885e76dadcff20e3b871fe2
dongeronimo <luciano.geronimo.fnord@gmail.com>
2020-06-11 20:22:43 -0300

    Copied the code from websocket-sharp to the game solution

---
+++ Assets/myAssets/Scenes/HelloWebSockets/ConnectionToServer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionToServer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //using (var ws = new WebSocket("ws://dragonsnest.far/Laputa"))
+        //{
+        //    ws.OnMessage += (sender, e) =>
+        //        Console.WriteLine("Laputa says: " + e.Data);
+        //    ws.Connect();
+        //    ws.Send("BALUS");
+        //    Console.ReadKey(true);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
