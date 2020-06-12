3d75ba3f84d10d3d3df5e5fed0ac78f1b5253cd6
dongeronimo <luciano.geronimo.fnord@gmail.com>
2020-06-12 02:21:29 -0300

    Walking around the world, still single player

--- Assets/myAssets/Scenes/SharedWorld/scripts/PlayerDesktopController.cs
+++ Assets/myAssets/Scenes/SharedWorld/scripts/PlayerDesktopController.cs
@@ -1,30 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDesktopController : MonoBehaviour
 {
-    public Vector3 currentOrientation;
+
     private CharacterController characterController;
     void Start()
     {
-
+  
         characterController = GetComponent<CharacterController>();
     }
     void Update()
     {
         SetCurrentOrientationUsingKeyboarInput();
-        CommitMovementChanges();
+        MoveForwardOrBackwardUsingKeyboardInput();
     }
+
+    private void MoveForwardOrBackwardUsingKeyboardInput()
+    {
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+        characterController.Move(transform.forward * verticalAxis * 3 * Time.deltaTime);
+    }
+
     void SetCurrentOrientationUsingKeyboarInput()
     {
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
-        float verticalAxis = Input.GetAxisRaw("Vertical");
-        
+        transform.Rotate(Vector3.up, 30 * horizontalAxis * Time.deltaTime);
     }
-    void CommitMovementChanges()
-    {
-        transform.LookAt(transform.position + currentOrientation);
 
-    }
+
 }
