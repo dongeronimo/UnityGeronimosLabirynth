0dd6bbde627766a56492025d021a3b001c920bb9
dongeronimo <luciano.geronimo.fnord@gmail.com>
2020-06-15 00:34:21 -0300

    At last I can get the ID in a webgl build. I had to find a websocket library compatible with webgl, adapt my code to this new library and then change my socket in the server to accept all origins to avoid error 403. Of course I will soon have to deal with socket security

--- server/MultiplayerServer/src/main/java/com/dongeronimo/multiplayer/teste/WebSocketConfig.java
+++ server/MultiplayerServer/src/main/java/com/dongeronimo/multiplayer/teste/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(helloSocketsHandler, "/helloSockets");
-        registry.addHandler(sharedWorldHandler, "/shared_world");
+        registry.addHandler(sharedWorldHandler, "/shared_world").setAllowedOrigins("*");
     }
     
 }
